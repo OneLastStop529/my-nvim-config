aaa86bcfa0fcdc98f6243bb2b8ba2ff8a22940b7
Yize Hu <hugohayes123@outlook.com>
2025-12-29 15:47:13 -0800

    keymaps change

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Toggle comment (VS Code style)
+-- macOS: Cmd + /
+vim.keymap.set("n", "<D-/>", "gcc", { remap = true, silent = true })
+vim.keymap.set("v", "<D-/>", "gc", { remap = true, silent = true })
+
+-- Linux / Windows: Ctrl + /
+vim.keymap.set("n", "<C-/>", "gcc", { remap = true, silent = true })
+vim.keymap.set("v", "<C-/>", "gc", { remap = true, silent = true })
