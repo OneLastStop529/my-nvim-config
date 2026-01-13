c125ff40952fb4ab59b7fec413b3f3546b987879
Yize Hu <hugohayes123@outlook.com>
2026-01-12 22:25:51 -0800

    adding toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    opts = {
+      open_mapping = [[<c-\>]],
+      shade_terminals = true,
+      shading_factor = 2,
+    },
+  },
+}
